e5079f5a8d049c7719f665ff5fd6cbad5052da06
Kris <Valentina10@sibmail.com>
2022-03-18 22:43:22 +0700

    Class Person

--- untitled/Person.h
+++ untitled/Person.h
@@ -1,8 +1,16 @@
+#include <string>
 
 class Person
 {
     private:
-
+        long key;
+        std::string last_name;
+        std::string first_name;
     public:
-
+        Person();
+        Person(const Person&);
+        const std::string& getLastName() const { return last_name;};
+        const std::string& getFirstName() const { return first_name;};
+        void setLastName(const std::string&);
+        void setFirstName(const std::string&);
 };

a626d9f5598163e5938af5edb19dd37b8adcada2
Kris <Valentina10@sibmail.com>
2022-03-18 21:35:50 +0700

    adding to class

--- untitled/stack.cpp
+++ untitled/stack.cpp
@@ -3,7 +3,27 @@ template <typename T>
 class STACK
 {
 private:
-
+    T* stack;
+    int count;
 public:
+    void push(T item)
+{
+        T* tmp;
+        tmp = stack;
+        stack = new T[count + 1];
 
+        count++;
+
+        for (int i = 0; i < count - 1; i++)
+        {
+            stack[i] = tmp[i];
+        }
+
+        stack[count - 1] = item;
+
+        if (count > 1)
+        {
+            delete[] tmp;
+        }
+    }
 };

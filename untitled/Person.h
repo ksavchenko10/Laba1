d664e455c4732bfa72b1327b67305eaa8b62fce7
Kris <Valentina10@sibmail.com>
2022-03-18 23:06:08 +0700

    adding to Person class

--- untitled/Person.h
+++ untitled/Person.h
@@ -14,3 +14,27 @@ class Person
         void setLastName(const std::string&);
         void setFirstName(const std::string&);
 };
+
+Person::Person()
+{
+    key = 1;
+    first_name = "NAME";
+    last_name = "SURNAME";
+}
+
+Person::Person(const Person& value)
+{
+    first_name = value.getFirstName();
+    last_name = value.getLastName();
+
+}
+
+void Person::setLastName(const std::string& value)
+{
+    last_name = value;
+}
+
+void Person::setFirstName(const std::string& value)
+{
+    first_name = value;
+}

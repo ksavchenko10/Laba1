e5a28c43936a8945e8d3ab581fe18e9bdd2335e9
Kris <Valentina10@sibmail.com>
2022-03-19 23:30:25 +0700

    Adding comments

--- untitled/laba1.cpp
+++ untitled/laba1.cpp
@@ -5,20 +5,20 @@
 
 int main()
 {
-    PersonKeeper personKeeperInstance;
+    PersonKeeper personKeeperInstance;	//создаем экземпляр класса personKeeperInstance
 
-        std::fstream file_in;
-        file_in.open("vvod.txt");
+    std::fstream file_in;
+    file_in.open("vvod.txt"); // открываем файл для чтения
 
-        STACK <Person> st;
-        st = *personKeeperInstance.readPersons(file_in);
+    STACK <Person> st;	//создаем стэк из элементов стека класса Person
+    st = *personKeeperInstance.readPersons(file_in); // метод readPersons возвращает стек с объектами класса Person
 
-        file_in.close();
+    file_in.close(); //закрывает поток чтения из файла
 
-        std::fstream file_out;
-        file_out.open("vyvod.txt");
+    std::fstream file_out;
+    file_out.open("vyvod.txt"); //открываем файл для записи
 
-        personKeeperInstance.writePersons(st, file_out);
+    personKeeperInstance.writePersons(st, file_out); //записываем в файл данные из стека
 
-        file_out.close();
+    file_out.close();
 }

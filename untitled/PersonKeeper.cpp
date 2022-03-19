0a9e41f7a4a59edb1456ce3a04dc4d41512e6b16
Kris <Valentina10@sibmail.com>
2022-03-19 23:08:52 +0700

    Adding comments

--- untitled/PersonKeeper.cpp
+++ untitled/PersonKeeper.cpp
@@ -3,14 +3,14 @@
 #include "Person.h"
 #include <iostream>
 
-PersonKeeper* PersonKeeper::singleton = NULL;
+PersonKeeper* PersonKeeper::singleton = NULL; //изначально экземпляр класса равен NULL, пока не будет объявлен
 
-PersonKeeper* PersonKeeper::instance()
+PersonKeeper* PersonKeeper::instance() //Реализация Singleton
 {
-    if (singleton == NULL)
+    if (singleton == NULL) //если экземпляра этого класса еще нет, то создаем его
     {
         singleton = new PersonKeeper;
     }
 
-    return singleton;
+    return singleton; //если экземпляр этого класса уже есть, отдаем его же
 }

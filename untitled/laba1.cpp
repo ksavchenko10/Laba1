2ed73170715c1cc5dfcfcb6a41f4cf7a79078dcf
Kris <Valentina10@sibmail.com>
2022-03-19 23:22:16 +0700

    Adding to Laba1

--- untitled/laba1.cpp
+++ untitled/laba1.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <fstream>
 #include "Person.h"
+#include "PersonKeeper.cpp"
 
 int main()
 {
+    PersonKeeper personKeeperInstance;
 
+        std::fstream file_in;
+        file_in.open("vvod.txt");
+
+        STACK <Person> st;
+        st = *personKeeperInstance.readPersons(file_in);
+
+        file_in.close();
+
+        std::fstream file_out;
+        file_out.open("vyvod.txt");
+
+        personKeeperInstance.writePersons(st, file_out);
+
+        file_out.close();
 }
